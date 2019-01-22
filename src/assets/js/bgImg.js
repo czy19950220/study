d65c038b1d09e186121c503badb2504b46ab74d0
czy19950220 <2772553223@qq.com>
2019-01-22 13:55:00 +0800

    2018-1-21

--- src/assets/js/bgImg.js
+++ src/assets/js/bgImg.js
@@ -1,38 +1,42 @@
-[
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项1"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项2"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项3"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项4"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项5"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项6"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项7"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项8"
-  },
-  {
-    "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
-    "label": "选择项9"
-  },
-]
+{
+  bgImg:
+  [
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项1"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项2"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项3"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项4"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项5"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项6"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项7"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项8"
+    },
+    {
+      "value": "https://czy-1257069199.cos.ap-beijing.myqcloud.com/my-app/novel/bg.jpg",
+      "label": "选择项9"
+    },
+  ]
+}
+
